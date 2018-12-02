ea847a6564a521e09980938b408dc1af079da436
Shota Yamazaki <sysy5867@gmail.com>
2018-12-02 10:21:56 +0900

    done section7

--- index.js
+++ index.js
@@ -6,17 +6,18 @@
 
 // AppRegistry.registerComponent(appName, () => App);
 
-
-
-
 // import a library to help create a component
 import React from 'react';
-import  { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import  { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // create a component
 const App = () => (
- <Header headerText={'albums'}/>
+    <View>
+        <Header headerText={'Albums'}/>
+        <AlbumList />
+    </View>
 );
 
 // render it to the device
