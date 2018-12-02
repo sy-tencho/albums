0a907125377eba60be66f4b24efbe5bd764ec39c
Shota Yamazaki <sysy5867@gmail.com>
2018-12-02 17:50:58 +0900

    done section8

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         height: 100,
-        paddingTop: 15,
+        paddingTop: 30,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 20},
         // shadowOpacity: 0.2,
